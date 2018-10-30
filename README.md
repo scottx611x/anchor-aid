3e086028b09fdb441685ea27f55554f69cff884a
Scott Ouellette <scottx611x@gmail.com>
2018-10-30 09:22:43 -0400

    Add some updates

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # anchor-aid
+
+# TODOS:
+- [ ] terraform code to create s3 bucket, and [custom IAM Roles for the lambda to be used by Zappa](https://github.com/Miserlou/Zappa#using-custom-aws-iam-roles-and-policies)
+- [ ] Implement Zappa portion of  code and utilize the role from previous step
+- [ ] Tests
+- [ ] Implement form for users to submit site + search query
+- [ ] Redirect user after successful POST to renderd site w/ highlight
+- [ ] return the url of the site + hash key of s3 to user
+- [ ] setup travis to test and deploy on master builds
+- [ ] Donation buttons?
+- [ ] App diagram?
--- app.py
+++ app.py
@@ -7,4 +7,23 @@ app.debug = True
 
 @app.route('/', methods=['GET'])
 def index():
+    return render_template("home.html", **{"site": None, "search": None})
+
+@app.route('/list', methods=['GET'])
+def list():
+    # List s3 bucket contents
+    raise NotImplementedError
+
+@app.route('/create', methods=['POST'])
+def create():
+    # generate config that will load a specific site and search query 
+        '''
+            {
+                "site": "en.wikipedia.org/wiki/Dog",
+                "search": "Beagles are cool"
+            }
+        '''
+    # generate hash based on content of config
+    # store config underneath hash key in s3
+
     return render_template("home.html")
