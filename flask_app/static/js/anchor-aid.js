0b176922ebdf25227496520427666437f9f4c457
Scott Ouellette <scottx611x@gmail.com>
2018-10-31 09:16:25 -0400

    Add empty travis file

---
+++ flask_app/static/js/anchor-aid.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    let urlParams = new URLSearchParams(window.location.search);
+    let url = "http://" + urlParams.get('url');
+    loadURL(url);
+};
+
+let getData = function (data) {
+    if (data && data.query && data.query.results && data.query.results.resources && data.query.results.resources.content && data.query.results.resources.status == 200) loadHTML(data.query.results.resources.content);
+    else if (data && data.error && data.error.description) loadHTML(data.error.description);
+    else loadHTML('Error: Cannot load ' + url);
+};
+
+let loadURL = function (src) {
+    let url = src;
+    let script = document.createElement('script');
+    script.src = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20data.headers%20where%20url%3D%22' + encodeURIComponent(url) + '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=getData';
+    document.body.appendChild(script);
+};
+
+let loadHTML = function (html) {
+    let jQ = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>';
+    let highlight = '<script src="/static/js/highlight.js"></script>';
+
+    let iframe = document.getElementById('site');
+    iframe.src = 'about:blank';
+    iframe.contentWindow.document.open();
+    iframe.contentWindow.document.write(html.replace(/<head>/i, '<head>' + jQ + highlight + '<base href="' + url + '"><scr' + 'ipt>document.addEventListener("click", function(e) { if(e.target && e.target.nodeName == "A") { e.preventDefault(); parent.loadURL(e.target.href); } });</scr' + 'ipt>'));
+    iframe.contentWindow.document.close();
+};
