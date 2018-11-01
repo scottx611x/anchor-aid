da6522a2b78ea91751c121bbce4eb5adf4815e81
Scott Ouellette <scottx611x@gmail.com>
2018-11-01 16:26:20 -0400

    Add tests

---
+++ flask_app/tests/test_app.py
@@ -0,0 +1,88 @@
+# coding=utf-8
+import uuid
+from contextlib import contextmanager
+from unittest import TestCase
+
+from flask import template_rendered
+
+from flask_app.app import app
+from flask_app.s3_bucket_config import S3_BUCKET
+
+import boto3
+from moto import mock_s3
+
+from flask_app.utils import dump_s3
+
+
+@contextmanager
+def captured_templates(app):
+    recorded = []
+
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
+
+
+class GenericTestBase(TestCase):
+    def setUp(self):
+        self.app = app
+        self.test_client = self.app.test_client()
+
+
+@mock_s3
+class RoutesTestCase(GenericTestBase):
+    def setUp(self):
+        super().setUp()
+        self.s3_client = boto3.client("s3")
+        self.s3_client.create_bucket(Bucket=S3_BUCKET)
+
+    def test_index_route(self):
+        with captured_templates(self.app) as templates:
+            response = self.test_client.get('/')
+        self.assertEquals(response.status_code, 200)
+        template, context = templates[0]
+        self.assertEqual(template.name, 'base.html')
+        self.assertIsNone(context.get("search"))
+        self.assertIsNone(context.get("site"))
+
+    def test_index_route_with_uuid(self):
+        key = str(uuid.uuid4())
+        data = {"search": "test", "site": "http://www.example.com"}
+        dump_s3(key, data)
+
+        with captured_templates(self.app) as templates:
+            response = self.test_client.get('/' + key)
+        self.assertEquals(response.status_code, 200)
+        template, context = templates[0]
+        self.assertEqual(template.name, 'anchor-aid.html')
+        self.assertIsNotNone(context.get("search"))
+        self.assertIsNotNone(context.get("site"))
+
+    def test_index_route_with_bad_uuid(self):
+        key = str(uuid.uuid4())
+        response = self.test_client.get('/' + key)
+        self.assertEquals(response.status_code, 400)
+
+    def test_list_route(self):
+        key = str(uuid.uuid4())
+        data = {"search": "test", "site": "http://www.example.com"}
+        dump_s3(key, data)
+        response = self.test_client.get('/list/' + key)
+        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.json, data)
+
+    def test_create_redirects_to_new_page(self):
+        data = {"search": "test", "site": "http://www.example.com"}
+        response = self.test_client.post('/create', data=data)
+        self.assertEquals(response.status_code, 302)
+
+    def test_create_route_bad_data(self):
+        data = {"search": "test", "site": ".com"}
+        response = self.test_client.post('/create', data=data)
+        self.assertEquals(response.status_code, 400)
+        self.assertEqual(response.data, b"Bad Request")
---
+++ flask_app/tests/test_utils.py
@@ -0,0 +1,64 @@
+from unittest import TestCase
+
+import boto3
+from moto import mock_s3
+
+from flask_app.s3_bucket_config import S3_BUCKET
+from flask_app.utils import is_valid_url, validate_form_data, dump_s3, load_s3
+
+
+class UtilitiesTestCase(TestCase):
+    def test_is_valid_url_good_url(self):
+        self.assertTrue(is_valid_url("http://www.example.com"))
+
+    def test_is_valid_url_bad_url(self):
+        self.assertFalse(is_valid_url("http:/www.example.com"))
+
+    def test_validate_form_data_good_data(self):
+        self.assertTrue(
+            validate_form_data(
+                {
+                    "site": "http://www.example.com",
+                    "search": "hello"
+                }
+            )
+        )
+
+    def test_validate_form_data_bad_site(self):
+        self.assertFalse(
+            validate_form_data(
+                {
+                    "site": "example.com",
+                    "search": "hello"
+                }
+            )
+        )
+
+    def test_validate_form_data_bad_search(self):
+        self.assertFalse(
+            validate_form_data(
+                {
+                    "site": "http://www.example.com",
+                    "search": {"hello": "world"}
+                }
+            )
+        )
+
+    def test_validate_form_data_no_site(self):
+        self.assertFalse(validate_form_data({"search": "hello"}))
+
+    def test_validate_form_data_no_search(self):
+        self.assertFalse(
+            validate_form_data({"site": "http://www.example.com"})
+        )
+
+
+@mock_s3
+class AWSUtilitiesTestCase(TestCase):
+    def setUp(self):
+        self.s3_client = boto3.client("s3")
+        self.s3_client.create_bucket(Bucket=S3_BUCKET)
+
+    def test_dump_and_load(self):
+        dump_s3("dog", {"name": "Fido"})
+        self.assertEqual(load_s3("dog")["name"], "Fido")
