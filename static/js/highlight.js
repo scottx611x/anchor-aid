a07e3091d8862a65dbe581664c3a0988bb8a01d7
Scott Ouellette <scottx611x@gmail.com>
2018-11-06 21:58:06 -0500

    Fix highlighting; ONly select a substring instead of the entire text node's contents

--- static/js/highlight.js
+++ static/js/highlight.js
@@ -1,13 +1,29 @@
 $( document ).ready(
     function() {
+        let highlightClassName = "anchor-aid-highlight";
         let anchors = document.getElementsByTagName('a');
         for (let i = 0; i < anchors.length; i++)
             anchors[i].target = '_blank';
         let $text = $('*:contains("' + parent.search + '"):last');
-        $text.css('background-color', 'yellow');
+        $text.highlight(parent.search, highlightClassName);
+        $("." + highlightClassName).css('background-color', 'yellow');
         $('html, body').animate({
             scrollTop: $text.offset().top * .9,
             scrollLeft: $text.offset().left
         });
+
     }
 );
+
+jQuery.fn.highlight = function (str, className) {
+    let regex = new RegExp(str, "gi");
+    return this.each(function () {
+        $(this).contents().filter(function() {
+            return this.nodeType === 3 && regex.test(this.nodeValue);
+        }).replaceWith(function() {
+            return (this.nodeValue || "").replace(regex, function(match) {
+                return "<span class=\"" + className + "\">" + match + "</span>";
+            });
+        });
+    });
+};
